26faafea850097cda440c7eab9ee0289c8570c3d
mirbek <mirbek.25.52@gmail.com>
2021-06-06 18:29:45 +0600

    Updates

---
+++ precache-manifest.aab2c82d731e241c4b3d5c0a41647a15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb77563bbcdedf28c5696181f13e41fd",
+    "url": "/cart/index.html"
+  },
+  {
+    "revision": "7b64c71c2826ba27ebb5",
+    "url": "/cart/static/css/main.b230c6c7.chunk.css"
+  },
+  {
+    "revision": "2829f658d05344f4543e",
+    "url": "/cart/static/js/2.4904b4fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cart/static/js/2.4904b4fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b64c71c2826ba27ebb5",
+    "url": "/cart/static/js/main.8878289c.chunk.js"
+  },
+  {
+    "revision": "6f930286e52d694f2280",
+    "url": "/cart/static/js/runtime-main.526553cb.js"
+  }
+]);
